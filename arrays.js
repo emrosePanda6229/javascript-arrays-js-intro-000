c0a82fe814d297726fa53baf62229e5660986b40
Emily R Collazo <ec889x@att.com>
2020-02-18 20:48:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
   var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-  chocolateBars = ['foo', ...chocolateBars];
+  //chocolateBars = ['foo', ...chocolateBars];
   return chocolateBars;
 }
 
